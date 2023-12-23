f0102d9724ce9987793fd526271c19d5e65e0d6d
laskhman <dineshsolankhi3@gmail.com>
2023-12-23 10:12:58 +0530

    first

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.myapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("this is for checking the github update commit","okay hai ");
     }
 }
